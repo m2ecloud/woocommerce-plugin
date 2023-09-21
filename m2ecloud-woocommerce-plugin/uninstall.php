dd18beaca29cdc1ba149fefe51cb2596f3b69f61
Roman Isaev <raisaev@users.noreply.github.com>
2023-09-21 08:58:54 +0300

    Delete m2ecloud-woocommerce-plugin directory

--- m2ecloud-woocommerce-plugin/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
-}
-
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-m2e-sc-helper.php';
-
-M2e_SC_Helper::clear_auth_data();
